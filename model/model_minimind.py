f3ead1abf3e250f5c4eb0d6b5b303335ac558d37
waozhang <1012925297@qq.com>
2026-07-03 22:16:15 +0800

    修改加实现FFN加打包

--- model/model_minimind.py
+++ model/model_minimind.py
@@ -67,6 +67,7 @@ class RMSNorm(torch.nn.Module):
     def norm(self, x):
         """
         标准差
+        mean：对最后一维求均方，会降维成一个标量，所以用keepdim=True广播回去
         """
         return x * torch.rsqrt(x.pow(2).mean(-1, keepdim=True) + self.eps)
 
@@ -122,7 +123,7 @@ def precompute_freqs_cid(dim: int, end: int = int(32 * 1024), rope_base: float =
     freqs_cos = torch.cat([torch.cos(freqs), torch.cos(freqs)], dim=-1) * attn_factor  # 乘注意力分数
     freqs_sin = torch.cat([torch.sin(freqs), torch.sin(freqs)], dim=-1) * attn_factor
     # return的结果应该是处理单个头的二维张量[seq_len,head_dim]
-    return freqs_cos, freqs_sin
+    return freqs_cos, freqs_sin # 返回一个处理单头的余弦正弦表
 
 
 def apply_rotary_pos_emb(q, k, cos, sin, unsqueeze_dim=1):
@@ -137,7 +138,7 @@ def apply_rotary_pos_emb(q, k, cos, sin, unsqueeze_dim=1):
     5、* 是哈达玛积，相同位置元素相乘，@是矩阵乘法
     """
 
-    def rotatle_half(x):
+    def rotate_half(x):
         """
         传入q或者k，把H维度从[x1,x2]转换成[-x2,x1]
         [a,b,c,d,e,f] -> [-d,-e,-f,a,b,c]
@@ -145,16 +146,20 @@ def apply_rotary_pos_emb(q, k, cos, sin, unsqueeze_dim=1):
         return torch.cat([-x[..., x.shape[-1] // 2:], x[..., :x.shape[-1] // 2]], dim=-1)
         # torch.cat的序列参数（第一个参数）可以用元组或列表
 
-    cos = cos.unsqueeze(unsqueeze_dim)  # 在维度1的位置插入一个维度，把[B,L,D]变成[B,H,L,D]
-    sin = sin.unsqueeze(unsqueeze_dim)
+    cos = cos.unsqueeze(unsqueeze_dim)  # 在维度1的位置插入一个维度，把[L,D]变成[L,1,D]
+    sin = sin.unsqueeze(unsqueeze_dim)  # 此时的cos和sin的维度是[L,1,D],qk的维度是[B,L,H,D]
     """
     哈达玛积广播：
     只有要广播的维度有一方维度数是1才能广播，其他情况都不行
     张量 1：[B, 1, L, D]，张量 2：[1, H, L, D] 可以广播
     张量 1：[1, 2, L, D]，张量 2：[B, H, L, D] 不能广播
     """
-    q_embed = ((q * cos) + (rotatle_half(q) * sin)).to(q.dtype)
-    k_embed = ((k * cos) + (rotatle_half(k) * sin)).to(k.dtype)
+    q_embed = ((q * cos) + (rotate_half(q) * sin)).to(q.dtype)
+    k_embed = ((k * cos) + (rotate_half(k) * sin)).to(k.dtype)
+    """
+                        形状的最后一块拼图   
+    四维的qk乘三维的cos,torch自动从-1维度对齐补全cos维度,并把值为1的维度广播
+    """
     # tensor.to(q.dtype) : 把张量数据类型转换成q中元素的数据类型
     # tensor.type_as(x) : 转换元素类型和所在设备，等价tensor.to(dtype=x.dtype, device=x.device)
     # np_arr.astype(np.float32) : numpy转换元素类型
@@ -188,15 +193,15 @@ class Attention(nn.Module):
         self.n_local_kv_heads = config.num_key_value_heads  # 放在本地显存的kv头
         self.n_rep = self.n_local_heads // self.n_local_kv_heads  # 一个kv对应几个q
         self.head_dim = config.head_dim  # 注意力头维度
-        self.is_causal = True  # 使用因果注意力掩码、
+        self.is_causal = True  # 使用因果注意力掩码
 
-        # 注意力权重矩阵QKV，一般WQ是方阵 即heads_size = num_kv_value_heads * head_dim
+        # 注意力权重矩阵QKV，一般Q是方阵 即heads_size = num_kv_value_heads * head_dim
         # WK、WV和WQ的输出差一个n_rep的倍数
         self.q_proj = nn.Linear(config.hidden_size, config.num_attention_heads * self.head_dim, bias=False)
         self.k_proj = nn.Linear(config.hidden_size, self.num_key_value_heads * self.head_dim, bias=False)
         self.v_proj = nn.Linear(config.hidden_size, self.num_key_value_heads * self.head_dim, bias=False)
         # O矩阵，输入多头拼接结果，融合多头的特征，输出矩阵做残差连接，一般是方阵
-        self.o_proj = nn.Linear(config.num_attention_heads * self.head_dim, config.hidden_size, bias=Fasle)
+        self.o_proj = nn.Linear(config.num_attention_heads * self.head_dim, config.hidden_size, bias=False)
         # 在算注意力分数前对q和k做标准化
         self.q_norm = RMSNorm(self.head_dim, eps=config.rms_norm_eps)
         self.k_norm = RMSNorm(self.head_dim, eps=config.rms_norm_eps)
@@ -206,22 +211,22 @@ class Attention(nn.Module):
         self.dropout = config.dropout
 
         # 闪存式高效注意力，硬件优化
-        self.flash = hasattr(torch.nn.funtional, 'scaled_dot_product_attention') and config.flash_attn
+        self.flash = hasattr(torch.nn.functional, 'scaled_dot_product_attention') and config.flash_attn
 
     def forward(self, x, position_embeddings, past_key_value=None, use_cache=False, attention_mask=None):
         bsz, seq_len, _ = x.shape
-        # 算权重矩阵
+        # 算权重矩阵------此时qkv形状：[batch_size, seq_len, dim]
         xq = self.q_proj(x)
         xk = self.k_proj(x)
         xv = self.v_proj(x)
-        # 拆分为多头
+        # 拆分为多头------此时q形状：[batch_size, seq_len, num_heads, head_dim]
         xq = xq.view(bsz, seq_len, self.n_local_heads, self.head_dim)
         xk = xk.view(bsz, seq_len, self.n_local_kv_heads, self.head_dim)
         xv = xv.view(bsz, seq_len, self.n_local_kv_heads, self.head_dim)
-        # 对qk标准化
+        # 对qk标准化------形状不变
         xq = self.q_norm(xq)
         xk = self.k_norm(xk)
-        # 位置编码
+        # 位置编码------形状不变
         cos, sin = position_embeddings
         xq, xk = apply_rotary_pos_emb(xq, xk, cos, sin)
 
@@ -243,12 +248,19 @@ class Attention(nn.Module):
             output = F.scaled_dot_product_attention(xq, xk, xv, dropout_p=self.dropout
                                                     if self.training
                                                     else 0.0, is_causal=self.is_causal)
-        else:   # 手动实现
-            scores = (xq @ xk.transpose(-2,-1))/math.sqrt(self.head_dim)    # q乘k转置除以维度的方根
-            # todo  没懂
+        else:
+            # 手动实现q乘k转置除以维度的方根
+            # [seq_q, dim] @ [dim, seq_k] = [seq_q, seq_k]
+            # scores.shape: [batch_size, num_heads, seq_q, seq_k]
+            scores = (xq @ xk.transpose(-2,-1))/math.sqrt(self.head_dim)
+            # todo1  懂了
             if self.is_causal:
+                # torch.full(...,-inf).triu: 上三角矩阵inf掩码，triu：上三角
+                # 把seq_k维度上的未来token加掩码，只对最后的seq_len 个token进行填充，刚好是一个上三角
                 scores[:,:,:,-seq_len:] += torch.full((seq_len, seq_len), float('-inf'), device=scores.device).triu(1)
             if attention_mask :
+                # 掩码pading的无效token
+                # attention_mask形状：[B, L] 在1，2维度升维 --> [B, 1, 1, L]
                 scores += (1.0 - attention_mask.unsqueeze(1).unsqueeze(2)) * -1e9
             output = self.attn_dropout(F.softmax(scores.float(), dim=-1).type_as(xq)) @ xv
         # 多头合并
@@ -256,3 +268,61 @@ class Attention(nn.Module):
         output = self.o_proj(output)    # 融合
         output = self.resid_dropout(output)     # dropout一下，准备残差
         return output, past_kv
+
+class FeedForward(nn.Module):
+    """
+    silu激活门控的前馈神经网络
+    """
+    def __init__(self, config: MiniMindConfig, intermediate_size: int = None):
+        super().__init__()
+        intermediate_size = intermediate_size or config.intermediate_size
+        self.gate_proj = nn.Linear(config.hidden_size, intermediate_size, bias=False)   # 走激活
+        self.down_proj = nn.Linear(intermediate_size, config.hidden_size, bias=False)
+        self.up_proj = nn.Linear(config.hidden_size, intermediate_size, bias=False)
+        self.act_fn = ACT2FN[config.hidden_act]
+
+    def forward(self, x):
+        return self.down_proj(self.up_proj(x) * self.act_fn(self.gate(x)))
+
+
+# todo MOEFFN
+class MOEFeedForward(nn.Module):
+    pass
+
+
+class MiniMindBlock(nn.Module):
+    """
+    一个decoder块，结构是:
+    h = x + Attention(RMSNorm(x));
+    out = h + MLP(RMSNorm(x)).
+    MLP可以是FFN或者MOEFFN
+    """
+    def __init__(self, layer_id: int, config: MiniMindConfig):
+        super().__init__()
+        self.self_attn = Attention(config)  # 自注意力层
+        self.input_layernorm = RMSNorm(config.hidden_size, eps=config.rms_norm_eps)     # 输入层归一化
+        self.post_attention_layernorm = RMSNorm(config.hidden_size, eps=config.rms_norm_eps)    # 注意力后层归一化
+        self.mlp = FeedForward(config) # if config.use_moe else MOEFeedForward(config)
+
+    def forward(self,hidden_states, position_embeddings, past_key_value=None, use_cache=False, attention_mask=None):
+        """
+        :param hidden_states: 隐藏状态
+        :param position_embeddings: 位置编码器
+        :param past_key_value: kv缓存
+        :param use_cache: 是否用kv缓存
+        :param attention_mask: 注意力掩码
+        """
+        residual = hidden_states
+        hidden_states, present_key_value = self.self_attn(
+            self.input_layernorm(hidden_states),
+            position_embeddings,
+            past_key_value=past_key_value,
+            use_cache=use_cache,
+            attention_mask=attention_mask
+        )   # 注意力层输出隐藏状态和kv缓存
+        hidden_states += residual
+        hidden_states = hidden_states + self.mlp(self.post_attention_layernorm(hidden_states))
+        return hidden_states, present_key_value # 返回隐藏状态和kv缓存
+
+class MiniMindModel(nn.Module):
+    pass
