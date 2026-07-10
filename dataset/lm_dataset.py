7536ce58e6cacec0d467d3a44e4dace5d955fc43
waozhang <1012925297@qq.com>
2026-07-10 21:35:16 +0800

    dataset start

---
+++ dataset/lm_dataset.py
@@ -0,0 +1,18 @@
+from torch.utils.data import Dataset
+import torch
+import json
+import os
+import random
+from datasets import load_dataset, Features, Sequence, Value
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
+
+class PretrainDataset(Dataset):
+    def __init__(self, data_path, tokenizer, max_length=512):
+        super().__init__()
+        self.tokenizer = tokenizer
+        self.max_length = max_length
+        self.samples = load_dataset('json', data_files=data_path, split='train')
+
+    def __len__(self):
+        return len(self.samples)
+
