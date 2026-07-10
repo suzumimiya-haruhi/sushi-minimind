03656e892abbc978910f144d3f3d29aa4c40a26e
waozhang <1012925297@qq.com>
2026-07-10 21:55:08 +0800

    pretrain dataset

--- dataset/lm_dataset.py
+++ dataset/lm_dataset.py
@@ -16,3 +16,15 @@ class PretrainDataset(Dataset):
     def __len__(self):
         return len(self.samples)
 
+    def __getitem__(self, index):
+        sample = self.samples[index]    # dataloader循环调用，每次传入一个index
+        tokens = self.tokenizer(sample['text'],
+                                add_special_token=False,
+                                max_length=self.max_length - 2,
+                                truncation=True).input_ids  # tokenizer返回一个复杂信息的字典，这里只取input ids键的值
+        tokens = [self.tokenizer.bos_token_id] + tokens + [self.tokenizer.eos_token_id]
+        input_ids = tokens + [self.tokenizer.pad_token_id] * (self.max_length - len(tokens))    # 批次填充，此input ids和上面的键不是一个东西
+        input_ids = torch.tensor(input_ids, dtype=torch.long)
+        labels = input_ids.clone()
+        labels[labels == self.tokenizer.pad_token_id] = -100    # 把pad部分的label设置成-100
+        return input_ids, labels
