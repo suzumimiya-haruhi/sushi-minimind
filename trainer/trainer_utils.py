13bcd5d5981cde8ddc493642465d7aac0577985b
waozhang <1012925297@qq.com>
2026-07-14 11:53:05 +0800

    加点工具

--- trainer/trainer_utils.py
+++ trainer/trainer_utils.py
@@ -13,32 +13,48 @@ from transformers import AutoTokenizer, AutoModel, AutoModelForSequenceClassific
 from model.model_minimind import MiniMindForCausalLM
 
 
-def get_lr():
-    pass
+def get_lr(current_step, total_step, lr):   #余弦退火学习率
+    return lr*(0.1 + 0.45*(1 + math.cos(math.pi * current_step/total_step)))    # 1倍到0.1倍lr
 
 
-def Logger():
-    pass
+def is_main_process():      # 判断是否是主进程
+    return not dist.is_initialized() or dist.get_rank() == 0
+    # (not dist.is_initialized()) or (dist.get_rank() == 0) 一边是true就为true
 
-
-def is_main_process():
-    pass
+def Logger(content):
+    if is_main_process():
+        print(content)  # 只打印主进程
 
 
 def lm_checkpoint():
     pass
 
 
-def init_distributed_mode():
-    pass
+def init_distributed_mode():    # 初始化分布式环境
+    if int(os.environ.get('RANK', -1)) == -1:
+        return 0
+
+    dist.init_process_group(backend='nccl')
+    local_rank = int(os.environ['LOCAL_RANK'])
+    torch.cuda.set_device(local_rank)
+    return local_rank
 
 
-def setup_seed():
-    pass
+def setup_seed(seed: int):   # 固定随机种子
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False      # 减少卷积算法选择带来的随机性
 
 
-def init_model():
-    pass
+# noinspection PyNoneFunctionAssignment
+def init_model(lm_config, from_weight='pretrain', tokenizer_path='../model',
+               save_dir='../out', device='cuda'):
+    tokenizer = AutoTokenizer.from_pretrained(tokenizer_path)   # 分词器，分词规则按照tokenizer_path，最长匹配优先
+
 
 
 class SkipBatchSampler(Sampler):
